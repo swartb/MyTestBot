a69f8d9c55e7d97d1f8798badc2d69f4a2438403
swartb <swartb@me.com>
2018-01-24 10:14:20 +0100

    Initial check-in of module TestConsoleApp1

---
+++ TestConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TestConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inLine;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Dit is een testapplicatie");
+            Console.WriteLine("Input: ");
+            inLine = Console.ReadLine();
+            Console.WriteLine("Output: {0}", inLine);
+            TestClass.myStruct mys;
+            mys.myInt = 0;
+            mys.myString = "abcd";
+            Console.WriteLine("test: {0} :::: {1}",mys.myInt,mys.myString);
+        }
+    }
+}
---
+++ TestConsoleApp1/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace TestConsoleApp1
+{
+    public class TestClass
+    {
+        public struct myStruct
+        {
+            public int myInt;
+            public string myString;
+
+
+        }
+      public TestClass()
+        {
+     
+
+        }
+    }
+}
